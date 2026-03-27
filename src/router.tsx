c4c2b69421a97718948514da5dfb4bef09214a7d
work <work@Thomas-MacBook-Pro.local>
2026-03-27 21:26:33 +0100

    fix: fix of base router

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HomePage />
+    }
+  ],
   {
-    path: '/',
-    element: <HomePage />
+    basename: import.meta.env.BASE_URL
   }
-]);
+);
